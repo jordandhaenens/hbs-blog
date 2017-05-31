1b225f723b0820419a9b6e0f68acda57a0846ad8
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-05-31 13:06:54 -0500

    finished with the formatting. MVP

--- js/main.js
+++ js/main.js
@@ -2,9 +2,16 @@
 
 let loader = require('./loader'),
 	template = require('../templates/blog.hbs'),
-	$ = require('jquery');
+	$ = require('jquery'),
+	Handlebars = require('hbsfy/runtime');
 // require('../lib/node_modules/materialize-css/dist/js/materialize.js');
 
+// Handlebars.registerHelper('Tarsis', (id, number) => {
+// 	console.log("id", id, "number", number);
+// 	if (parseInt(number, 10) === id){
+// 		return true;
+// 	}
+// });
 
 function buildArticle(data){
 	let article = template(data);
